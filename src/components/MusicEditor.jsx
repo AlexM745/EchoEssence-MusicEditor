ba025bb7c4c530eb48e5a741f21505a6e2ea187c
Alexis Merino <alexismerino49@gmail.com>
2024-08-01 22:05:38 -0500

    added components and started MusicEditor

---
+++ src/components/MusicEditor.jsx
@@ -0,0 +1,14 @@
+import {useEffect, useRef} from "react";
+import { Renderer, Stave, StaveNote, Voice, Formatter } from "vexflow";
+import * as Tone from 'tone';
+
+const MusicEditor =()=> {
+    return (
+        <p> Music Editor</p>
+    )
+
+}
+
+
+
+export default MusicEditor;
