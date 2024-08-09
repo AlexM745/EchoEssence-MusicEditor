fff986cba9a7cab6fc07ae734af7c92814fac842
Alexis Merino <alexismerino49@gmail.com>
2024-08-08 22:47:02 -0500

    starting adding logic so that a midi keyboard can  work

--- src/components/MusicEditor.jsx
+++ src/components/MusicEditor.jsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Renderer, Stave, StaveNote, Voice, Formatter } from 'vexflow';
-import {WebMidi} from 'webmidi';
+import { WebMidi } from 'webmidi';
 import * as Tone from 'tone';
 import { Vex } from "vexflow";
 
 const MusicEditor = () => {
 
+    // This hooks  managees the notes, context, stave so that they can be updated and accessed
     const containerRef = useRef(null);
     const [notes, setNotes] = useState([]);
+    const [context, setContext] = useState(null);
+    const [stave, setStave] = useState(null);
+
 
     useEffect(() => {
+
         const VF = Vex.Flow;
         const div = containerRef.current;
+        // initializes the vexflow renderer to draw and svg
         const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
         // this is how big the stave or music is going to render on the page
         renderer.resize(500, 200);
+        // used to draw on the canvas or svg using basic font and background
         const context = renderer.getContext();
         context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed');
 
@@ -23,24 +30,28 @@ const MusicEditor = () => {
         stave.addClef('treble').addTimeSignature('4/4');
         stave.setContext(context).draw();
 
+        setContext(context);
+        setStave(stave);
+
         // if there are notes then create the voice
-        if (notes.length > 0){
-        //  This is the number of notes and value in each measure
-        const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-        voice.addTickables(notes);
+        if (notes.length > 0) {
+            //  This is the number of notes and value in each measure
+            const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+            voice.addTickables(notes);
 
-        new VF.Formatter().joinVoices([voice]).format([voice], 400);
+            new VF.Formatter().joinVoices([voice]).format([voice], 400);
 
-        voice.draw(context, stave);
+            voice.draw(context, stave);
         }
 
-        // Setup WebMidi
+        // Setup WebMidi to be able to use MIDI input devices
+
         WebMidi.enable((err) => {
             if (err) {
                 console.error("WebMidi could not be enabled.", err);
             } else {
                 console.log("WebMidi enabled!");
-
+                // When a note is played on a MIDI it calls the addNotePlay function
                 WebMidi.inputs.forEach((input) => {
                     input.addListener('noteon', 'all', (e) => {
                         const note = e.note.name.toLowerCase() + '/' + e.note.octave;
@@ -50,20 +61,20 @@ const MusicEditor = () => {
             }
         });
 
-    }, [notes]);
+    }, []);
 
     // this is so that the user can add and play the note added
     const addNoteAndPlay = (note) => {
-        setNotes([...notes, new StaveNote({
+        const newNote = new StaveNote({
             keys: [note],
             duration: 'q',
-        })]);
+        });
         // for the new notes
         setNotes(prevNotes => [...prevNotes, newNote]);
-
+        // uses Tone js to map the note 
         playNote(note);
     };
-    // this is so that the notes are played 
+    // this is so that the notes are played with tone.js
     const playNote = (note) => {
         const synth = new Tone.Synth().toDestination();
         const noteMap = {
@@ -80,11 +91,29 @@ const MusicEditor = () => {
             synth.triggerAttackRelease(pitch, '8n');
         }
     };
-    // for when the user adds notes by mouse click
+    // for when the user adds notes by mouse click by capturing the y corrdinate of the click
     const handleMouseClick = (e) => {
-        // need to add logic to determine the note based on the click position
-        // fixed note as of now
-        addNoteAndPlay('c/4');
+        const rect = containerRef.current.getBoundingClientRect();
+        const y = e.clientY - rect.top; // checking where the y coordinate is relative to the stave 
+        const note = getNoteFromY(y); // note is  y cordinate 
+        // if a note is identified then it is added
+        if (note) {
+            addNoteAndPlay(note);
+        }
+    };
+
+
+    const getNoteFromY = (y) => {
+        const staveTop = 40; // The y-coordinate where the stave starts
+        const noteHeight = 10; // Approximate height of a note step on the stave
+
+        // Map y-coordinate to note
+        if (y >= staveTop && y < staveTop + 5 * noteHeight) {
+            const notePositions = ['f/4', 'e/4', 'd/4', 'c/4', 'b/3'];
+            const index = Math.floor((y - staveTop) / noteHeight);
+            return notePositions[index];
+        }
+        return null;
     };
 
     return (
