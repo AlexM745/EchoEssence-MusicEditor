be6851326f96e2011e2a430182cd455c0a20ebff
Alexis Merino <alexismerino49@gmail.com>
2024-08-11 20:54:53 -0500

    mapped all the 88 keys of a piano for the midi keyboard

--- src/components/MusicEditor.jsx
+++ src/components/MusicEditor.jsx
@@ -51,16 +51,16 @@ const MusicEditor = () => {
                 console.error("WebMidi could not be enabled.", err);
             } else {
                 console.log("WebMidi enabled!");
-                // When a note is played on a MIDI it calls the addNotePlay function
+
                 WebMidi.inputs.forEach((input) => {
                     input.addListener('noteon', 'all', (e) => {
-                        const note = e.note.name.toLowerCase() + '/' + e.note.octave;
-                        addNoteAndPlay(note);
+                        // when MIDI notes is received the number of the key is passed to playnote
+                        // tp correctly map the note and play it.
+                        playNote(e.note.number);
                     });
                 });
             }
         });
-
     }, []);
 
     // this is so that the user can add and play the note added
@@ -74,21 +74,39 @@ const MusicEditor = () => {
         // uses Tone js to map the note 
         playNote(note);
     };
+    // these are all the notes which thier corresponding pitch by key number.
+    const midiNoteToPitch = {
+        21: 'A0', 22: 'A#0', 23: 'B0',
+        24: 'C1', 25: 'C#1', 26: 'D1', 27: 'D#1', 28: 'E1', 29: 'F1', 30: 'F#1', 31: 'G1', 32: 'G#1',
+        33: 'A1', 34: 'A#1', 35: 'B1',
+        36: 'C2', 37: 'C#2', 38: 'D2', 39: 'D#2', 40: 'E2', 41: 'F2', 42: 'F#2', 43: 'G2', 44: 'G#2',
+        45: 'A2', 46: 'A#2', 47: 'B2',
+        48: 'C3', 49: 'C#3', 50: 'D3', 51: 'D#3', 52: 'E3', 53: 'F3', 54: 'F#3', 55: 'G3', 56: 'G#3',
+        57: 'A3', 58: 'A#3', 59: 'B3',
+        60: 'C4', 61: 'C#4', 62: 'D4', 63: 'D#4', 64: 'E4', 65: 'F4', 66: 'F#4', 67: 'G4', 68: 'G#4',
+        69: 'A4', 70: 'A#4', 71: 'B4',
+        72: 'C5', 73: 'C#5', 74: 'D5', 75: 'D#5', 76: 'E5', 77: 'F5', 78: 'F#5', 79: 'G5', 80: 'G#5',
+        81: 'A5', 82: 'A#5', 83: 'B5',
+        84: 'C6', 85: 'C#6', 86: 'D6', 87: 'D#6', 88: 'E6', 89: 'F6', 90: 'F#6', 91: 'G6', 92: 'G#6',
+        93: 'A6', 94: 'A#6', 95: 'B6',
+        96: 'C7', 97: 'C#7', 98: 'D7', 99: 'D#7', 100: 'E7', 101: 'F7', 102: 'F#7', 103: 'G7', 104: 'G#7',
+        105: 'A7', 106: 'A#7', 107: 'B7',
+        108: 'C8'
+    };
+
     // this is so that the notes are played with tone.js
-    const playNote = (note) => {
+    const playNote = (noteNumber) => {
+
+        console.log(`Received note: ${noteNumber}`);
+
         const synth = new Tone.Synth().toDestination();
-        const noteMap = {
-            'c/4': 'C4',
-            'd/4': 'D4',
-            'e/4': 'E4',
-            'f/4': 'F4',
-            'g/4': 'G4',
-            'a/4': 'A4',
-            'b/4': 'B4',
-        };
-        const pitch = noteMap[note];
+        const pitch = midiNoteToPitch[noteNumber];
+
         if (pitch) {
+            console.log(`Playing pitch: ${pitch}`);
             synth.triggerAttackRelease(pitch, '8n');
+        } else {
+            console.error(`Note number ${noteNumber} is not mapped.`);
         }
     };
     // for when the user adds notes by mouse click by capturing the y corrdinate of the click
